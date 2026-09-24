20cd199a5f50a99402e921ccda80641c43697186
qiqi <yizebaba@gmail.com>
2026-09-24 14:06:29 +0800

    chore: bump version to 3.1.0

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces.d';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-everest-mhews',
-    version: '3.0.7',
+    version: '3.1.0',
     icon: '?',
     title: 'Everest Glacier Experimental Analysis',
     description: 'Experimental multi-sensor screening of provisional Mount Everest glacier movement candidates.',
